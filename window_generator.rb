5f5a7ccc4b00dc51811d1cb257e3e517593a320a
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2015-08-07 09:33:00 -0500

    Two new strategies

--- window_generator.rb
+++ window_generator.rb
@@ -1,27 +1,48 @@
 class WindowGenerator
-  STRATEGIES = [:clone]
+  STRATEGIES = [:clone, :new_array, :concat]
 
   def initialize(arr, window_size)
     @arr = arr
     @window_size = window_size
   end
 
-  def via(strategy)
+  def via(strategy, &block)
     unless STRATEGIES.include? strategy
       fail ArgumentError, "Unsupported option #{strategy.inspect.to_s}"
     end
+    send("via_#{strategy}", &block)
   end
 
   private
-  def windows(strategy)
-    return to_enum(__callee__, arr, window_size) unless block_given?
+  def via_clone
+    return to_enum(__callee__) unless block_given?
     prefix = arr.slice(0, window_size-1)
     arr[(window_size-1)..-1].each do |elt|
       prefix << elt
-      yield prefix #.clone
+      yield prefix.clone
       prefix.delete_at 0
     end
   end
 
-  attr_accessor :digits, :window_size
+  def via_new_array
+    return to_enum(__callee__) unless block_given?
+    prefix = arr.slice(0, window_size-1)
+    arr[(window_size-1)..-1].each do |elt|
+      prefix << elt
+      yield Array.new(prefix)
+      prefix.delete_at 0
+    end
+  end
+
+  def via_concat
+    return to_enum(__callee__) unless block_given?
+    prefix = arr.slice(0, window_size-1)
+    arr[(window_size-1)..-1].each do |elt|
+      yield prefix + [elt]
+      prefix << elt
+      prefix.delete_at 0
+    end
+  end
+
+  attr_accessor :arr, :window_size
 end
